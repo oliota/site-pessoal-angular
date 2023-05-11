f8449f5f1d1d3d130915c04f92e129e893f452c1
Rubem Oliota <rubemoliota@gmail.com>
2023-05-11 16:47:37 -0300

    update version site

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'; 
 
 @Component({
   selector: 'app-inicio',
@@ -12,7 +12,13 @@ export class InicioComponent implements OnInit {
 
   ngOnInit(): void {
     this.mobile = window.screen.width <= 700
-    
+    let modo_desenvolvedor: any = sessionStorage.getItem("modo_desenvolvedor")
+ 
+
+    if(!modo_desenvolvedor) {
+      sessionStorage.setItem("modo_desenvolvedor", JSON.stringify(false)) 
+    }  
+
 
 
   }
