5de2e756d333e4bebdc519694556c4fbe7e3f8b5
Rubem Oliota <rubemoliota@gmail.com>
2023-05-12 11:30:12 -0300

    calculadora de gastos - conversao de valores

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from  '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 //import {Nl2BrPipeModule} from 'nl2br-pipe';
@@ -39,6 +39,7 @@ import { CalculadoraCustosRendaInternacionalComponent } from './utilitarios/calc
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
+    ReactiveFormsModule,
     MDBBootstrapModule.forRoot()
   ],
   providers: [
--- src/app/utilitarios/calculadora-custos-renda-internacional/calculadora-custos-renda-internacional.component.ts
+++ src/app/utilitarios/calculadora-custos-renda-internacional/calculadora-custos-renda-internacional.component.ts
@@ -11,32 +11,130 @@ import { ClassConsole } from 'src/app/easterEggs/ClassConsole';
 })
 export class CalculadoraCustosRendaInternacionalComponent implements OnInit {
 
-  rendas: any = { 
-    conversoes:{}
+  valorInicial: number = 10000
+  rendas: any = {
+    real: { disabled: false, value: this.valorInicial },
+    euro: { disabled: true, value: this.valorInicial },
+    dolar: { disabled: true, value: this.valorInicial },
+    conversoes: {
+      real: 0,
+      euro: 0,
+      dolar: 0
+    }
+
   }
+  cotacoes!: any
+  rendaPrincipal: any = "real"
 
   oliotaUtils!: OliotaUtils
   constructor(oliota: OliotaUtils,
-    private cotacaoService:CotacaoMonetariaService
-    ) {
+    private cotacaoService: CotacaoMonetariaService
+  ) {
     this.oliotaUtils = oliota
   }
 
   ngOnInit(): void {
 
-     this.cotacaoService.getCotacaoes( ).subscribe((data: {}) => {
-      const response: any = data;
-       ClassConsole.log("getCotacaoes",response)
+    this.cotacaoService.getCotacaoes().subscribe((data: {}) => {
+      this.cotacoes = data;
+      ClassConsole.log("getCotacaoes", this.cotacoes)
+      console.log("this.rendas.real.value",this.rendas.real.value);
+       
+        this.changeRenda(this.rendas.real.value)  
     }, (error: {}) => {
-      
-    }); 
+
+    });
 
   }
 
-  changeRenda(target:any){
-console.log("changeRenda",target.value)
+
+  changeRadioRenda(event: any) {
+    console.log("changeRadioRenda", event.target.value);
+    this.rendaPrincipal = event.target.value
+
+    // let valor: any = {
+    //   target: {
+    //     value: this.rendas[this.rendaPrincipal]
+    //   }
+    // }
+    this.changeRenda(this.rendas[this.rendaPrincipal].value) 
+    switch (this.rendaPrincipal) {
+      case "real":
+        this.rendas.real.disabled = false;
+        this.rendas.euro.disabled = true;
+        this.rendas.dolar.disabled = true;
+        
+        break;
+      case "euro":
+        this.rendas.real.disabled = true;
+        this.rendas.euro.disabled = false;
+        this.rendas.dolar.disabled = true;
+        break;
+      case "dolar":
+        this.rendas.real.disabled = true;
+        this.rendas.euro.disabled = true;
+        this.rendas.dolar.disabled = false;
+        break;
+
+      default:
+        break;
+    }
+
+
 
   }
-   
+
+
+
+
+
+  changeRenda(event: any) {
+    let valor:any=(event.target?.value?.value)?event.target.value.value:event
+    
+    console.log("changeRenda", valor)
+    let conversoes = this.converterValor(valor)
+    this.rendas.conversoes.real = conversoes.real
+    this.rendas.conversoes.euro = conversoes.euro
+    this.rendas.conversoes.dolar = conversoes.dolar
+
+
+  }
+
+  converterValor(valor: any) {
+    let retorno: any = {};
+console.log("converterValor",valor);
+
+    valor = Number(valor.toString().replace(".", "").replace(",", "."))
+    
+console.log("converterValor",valor);
+
+    console.log("valor", valor);
+    console.log("this.rendaPrincipal", this.rendaPrincipal);
+
+    switch (this.rendaPrincipal) {
+      case "real":
+        retorno.real = valor;
+        retorno.euro = valor / this.cotacoes["EURBRL"]["ask"];
+        retorno.dolar = valor / this.cotacoes["USDBRL"]["ask"];
+        break;
+      case "euro":
+        retorno.real = valor * this.cotacoes["EURBRL"]["ask"];
+        retorno.euro = valor;
+        retorno.dolar = retorno.real / this.cotacoes["USDBRL"]["ask"];
+        break;
+      case "dolar":
+        retorno.real = valor * this.cotacoes["USDBRL"]["ask"];
+        retorno.euro = retorno.real / this.cotacoes["EURBRL"]["ask"];
+        retorno.dolar = valor;
+        break;
+
+      default:
+        break;
+    }
+    console.log("converterValor", retorno)
+    return retorno
+
+  }
+
 
 }
