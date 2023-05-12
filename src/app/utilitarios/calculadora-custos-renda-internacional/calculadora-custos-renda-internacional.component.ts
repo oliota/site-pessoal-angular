93a0186866f0c03386c4ea68da4822440672be2e
Rubem Oliota <rubemoliota@gmail.com>
2023-05-12 16:46:08 -0300

    campos inteiros

--- src/app/utilitarios/calculadora-custos-renda-internacional/calculadora-custos-renda-internacional.component.ts
+++ src/app/utilitarios/calculadora-custos-renda-internacional/calculadora-custos-renda-internacional.component.ts
@@ -26,6 +26,9 @@ export class CalculadoraCustosRendaInternacionalComponent implements OnInit {
   cotacoes!: any
   rendaPrincipal: any = "real"
 
+  itens: any = []
+  item:any={}
+
   oliotaUtils!: OliotaUtils
   constructor(oliota: OliotaUtils,
     private cotacaoService: CotacaoMonetariaService
@@ -38,9 +41,9 @@ export class CalculadoraCustosRendaInternacionalComponent implements OnInit {
     this.cotacaoService.getCotacaoes().subscribe((data: {}) => {
       this.cotacoes = data;
       ClassConsole.log("getCotacaoes", this.cotacoes)
-      console.log("this.rendas.real.value",this.rendas.real.value);
-       
-        this.changeRenda(this.rendas.real.value)  
+      console.log("this.rendas.real.value", this.rendas.real.value);
+
+      this.changeRenda(this.rendas.real.value)
     }, (error: {}) => {
 
     });
@@ -57,13 +60,13 @@ export class CalculadoraCustosRendaInternacionalComponent implements OnInit {
     //     value: this.rendas[this.rendaPrincipal]
     //   }
     // }
-    this.changeRenda(this.rendas[this.rendaPrincipal].value) 
+    this.changeRenda(this.rendas[this.rendaPrincipal].value)
     switch (this.rendaPrincipal) {
       case "real":
         this.rendas.real.disabled = false;
         this.rendas.euro.disabled = true;
         this.rendas.dolar.disabled = true;
-        
+
         break;
       case "euro":
         this.rendas.real.disabled = true;
@@ -89,8 +92,8 @@ export class CalculadoraCustosRendaInternacionalComponent implements OnInit {
 
 
   changeRenda(event: any) {
-    let valor:any=(event.target?.value?.value)?event.target.value.value:event
-    
+    let valor: any = (event.target?.value?.value) ? event.target.value.value : event
+
     console.log("changeRenda", valor)
     let conversoes = this.converterValor(valor)
     this.rendas.conversoes.real = conversoes.real
@@ -102,30 +105,30 @@ export class CalculadoraCustosRendaInternacionalComponent implements OnInit {
 
   converterValor(valor: any) {
     let retorno: any = {};
-console.log("converterValor",valor);
+    console.log("converterValor", valor);
 
     valor = Number(valor.toString().replace(".", "").replace(",", "."))
-    
-console.log("converterValor",valor);
+
+    console.log("converterValor", valor);
 
     console.log("valor", valor);
     console.log("this.rendaPrincipal", this.rendaPrincipal);
 
     switch (this.rendaPrincipal) {
       case "real":
-        retorno.real = valor;
-        retorno.euro = valor / this.cotacoes["EURBRL"]["ask"];
-        retorno.dolar = valor / this.cotacoes["USDBRL"]["ask"];
+        retorno.real = (valor).toFixed(2);
+        retorno.euro = (valor / this.cotacoes["EURBRL"]["ask"]).toFixed(2);
+        retorno.dolar = (valor / this.cotacoes["USDBRL"]["ask"]).toFixed(2);
         break;
       case "euro":
-        retorno.real = valor * this.cotacoes["EURBRL"]["ask"];
-        retorno.euro = valor;
-        retorno.dolar = retorno.real / this.cotacoes["USDBRL"]["ask"];
+        retorno.real = (valor * this.cotacoes["EURBRL"]["ask"]).toFixed(2);
+        retorno.euro = (valor).toFixed(2);
+        retorno.dolar = (retorno.real / this.cotacoes["USDBRL"]["ask"]).toFixed(2);
         break;
       case "dolar":
-        retorno.real = valor * this.cotacoes["USDBRL"]["ask"];
-        retorno.euro = retorno.real / this.cotacoes["EURBRL"]["ask"];
-        retorno.dolar = valor;
+        retorno.real = (valor * this.cotacoes["USDBRL"]["ask"]).toFixed(2);
+        retorno.euro = (retorno.real / this.cotacoes["EURBRL"]["ask"]).toFixed(2);
+        retorno.dolar = (valor).toFixed(2);
         break;
 
       default:
@@ -133,6 +136,42 @@ console.log("converterValor",valor);
     }
     console.log("converterValor", retorno)
     return retorno
+
+  }
+
+  adicionar(){
+
+
+    
+    let conversao:any = this.converterValor(this.item.value );
+
+    this.item.real=this.calcularPercentual(this.rendas.real,conversao.real)
+    this.item.euro=this.calcularPercentual(this.rendas.euro,conversao.euro)
+    this.item.dolar=this.calcularPercentual(this.rendas.dolar,conversao.dolar) 
+
+    this.itens.push(Object.assign({}, this.item) )
+    console.log("itens",this.itens);
+    
+    this.item={}
+
+  }
+
+  calcularPercentual(renda:any,valorConvertido:any){
+    console.log("calcularPercentual",renda,valorConvertido)
+    renda=Number(renda.value.toString().replace(".","").replace(",","."))
+    valorConvertido=Number(valorConvertido.toString().replace(".","").replace(",","."))
+
+    
+    if(renda && renda>0){
+      let calculo=(valorConvertido/renda)*100
+      console.log("calcularPercentual",valorConvertido,"/",renda,"*",100,"=",calculo);
+      return {
+        conversao:valorConvertido.toFixed(2),
+        percentual:calculo.toFixed(2)
+      }
+    }else{ 
+      return null
+    }
 
   }
 
